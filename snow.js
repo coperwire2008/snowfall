a3c4c3a77cfa3923d98003d4840196e2daa1b300
coperwire2008 <75265672+coperwire2008@users.noreply.github.com>
2021-06-19 17:07:38 +0530

    Add files via upload

---
+++ snow.js
@@ -0,0 +1,30 @@
+class snow    {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:false
+
+        }
+        this.r = 30;
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.image = loadImage("images/snow5.webp")
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill("white");
+        imageMode(RADIUS);
+        image(this.image,0,0,this.r,this.r);
+        pop();
+    }
+
+}; 
